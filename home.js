8f84ec56c8accafa06515e4cb0fb5a89b270499c
Your Name <you@example.com>
2021-01-23 06:53:05 -0800

    home.work.1

--- home.js
+++ home.js
@@ -1,11 +1,58 @@
-
- let unit = prompt("tochka zamerzanie body ? bbedite edinisu izmereniya:" )
- let temprature = prompt ("vvedite gradus:");
-     if (unit == "C" ||unit == "k" ||  unit == "F" )  {
-      if (temprature == 0 || temprature == 233 || temprature ==1 ){
+/*
+ let year = prompt("when born Kendji Jirack ? vvedite year " )
+ let day = prompt ("vvedite day:");
+     if (year == "1990" || year == "1992" ||  year == "1999" )  {
+     if (day == 20 || day == 23 || day == 21 ){
              console.log("pravilno")
             }
             else{
                 console.log("nepravilno")
             }
         }
+*/
+/*
+     let year = prompt("when died darvin ? vvedite year " )
+     let month = prompt ("vvedite month :");
+         if (year == "1890" || year == "1850" ||  year == "1880" )  {
+         if (month == april || month == may || month == march ){
+                    console.log("pravilno")
+                   }
+                   else{
+                       console.log("nepravilno")
+                   }
+               }
+
+*/
+
+
+let  mamleket  = prompt("1999 dan 2005 jylga cheiin kendji kaisy mamlekette bolgon")
+switch (mamleket) {
+     case "kyrgyz":
+     case "kyrgyzstan":
+     case "kyrgyzrespublick":
+      case "kirgiz":
+          console.log("2000")
+       break;
+
+       case "russkii":
+       case "russia":
+        console.log("2001")
+        break;
+
+        case "english":
+        case "england":
+        console.log("1999")
+        break;
+
+       default:
+
+       console.log("my ne znaem takoi strany");
+
+       break;
+}
+
+
+
+
+
+
